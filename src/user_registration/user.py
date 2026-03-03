10d4a2e7c1414b67a76685b82165803b423ce6ee
Shengjie Ma <mashengjie73@gmail.com>
2026-03-03 13:41:57 -0800

    update interface

--- src/user_registration/user.py
+++ src/user_registration/user.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class User:
-    """Class representing a user."""
+    """Class representing a user holding bananas."""
 
-    username_xyz: str
     banana_cnt: int
+    username: str
 
 
 def register_user(date: str, user: User) -> None:
     """Register a user."""
-    print(f"Registering user {user.username_xyz} with {user.banana_cnt} bananas on {date}.")
+    print(f"Registering user {user.username} with {user.banana_cnt} bananas on {date}.")
